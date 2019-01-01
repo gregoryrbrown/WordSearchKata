3581fc394e1fb61c9cbc5922161da0b5851087e4
Gregory Brown <gregory@associationvoting.com>
2019-01-01 10:46:35 -0500

    Adding a second test for the up and to the right direction searching.

--- WordSearchTest/WordSearchTest.cs
+++ WordSearchTest/WordSearchTest.cs
@@ -80,6 +80,7 @@ namespace WordSearchTest
         
         [Theory]
         [InlineData(1)]
+        [InlineData(6)]
         public void WordSearch_Finds_Word_in_up_and_to_right_direction(int testCase)
         {
             WordSearch.WordSearch wordFinder = new WordSearch.WordSearch();
@@ -161,6 +162,13 @@ namespace WordSearchTest
                 return "(7,4),(8,3),(9,2),(10,1),(11,0)";
             }
 
+            
+            if (6 == testCase )
+            {
+                return "(10,14),(11,13),(12,12),(13,11),(14,10)";
+            }
+            
+            
             
             return "";
         }
@@ -283,6 +291,27 @@ namespace WordSearchTest
             }
 
             
+            if (6 == testCase)
+            {
+                return new List<List<char>>
+                {
+                    new List<char> {'U', 'N', 'K', 'S', 'G', 'K', 'M', 'A', 'R', 'N', 'H', 'X', 'D', 'T', 'A'},
+                    new List<char> {'W', 'T', 'F', 'E', 'C', 'Q', 'M', 'W', 'Q', 'J', 'C', 'I', 'H', 'S', 'N'},
+                    new List<char> {'V', 'V', 'Q', 'N', 'K', 'M', 'Y', 'C', 'Y', 'O', 'W', 'R', 'J', 'W', 'N'},
+                    new List<char> {'M', 'Q', 'E', 'O', 'R', 'H', 'B', 'T', 'P', 'Y', 'E', 'K', 'Y', 'V', 'I'},
+                    new List<char> {'J', 'W', 'V', 'B', 'B', 'Y', 'A', 'S', 'T', 'Y', 'O', 'M', 'L', 'O', 'N'},
+                    new List<char> {'J', 'R', 'L', 'M', 'C', 'U', 'C', 'N', 'O', 'O', 'W', 'U', 'U', 'W', 'X'},
+                    new List<char> {'X', 'O', 'E', 'N', 'X', 'M', 'Y', 'S', 'V', 'I', 'C', 'S', 'R', 'Q', 'O'},
+                    new List<char> {'K', 'Z', 'R', 'P', 'H', 'N', 'T', 'P', 'W', 'H', 'D', 'S', 'R', 'P', 'R'},
+                    new List<char> {'Y', 'K', 'L', 'W', 'Z', 'T', 'M', 'Q', 'H', 'U', 'H', 'U', 'S', 'A', 'K'},
+                    new List<char> {'D', 'D', 'O', 'I', 'H', 'H', 'N', 'P', 'M', 'C', 'J', 'N', 'O', 'K', 'V'},
+                    new List<char> {'Y', 'X', 'X', 'I', 'V', 'K', 'E', 'G', 'A', 'G', 'J', 'P', 'M', 'P', 'K'},
+                    new List<char> {'A', 'K', 'M', 'L', 'V', 'Y', 'E', 'I', 'J', 'C', 'U', 'S', 'S', 'C', 'N'},
+                    new List<char> {'W', 'M', 'S', 'F', 'N', 'W', 'S', 'S', 'P', 'V', 'J', 'U', 'O', 'I', 'I'},
+                    new List<char> {'C', 'V', 'Z', 'C', 'Z', 'B', 'Y', 'O', 'X', 'K', 'L', 'P', 'E', 'Z', 'O'},
+                    new List<char> {'K', 'T', 'E', 'T', 'T', 'C', 'E', 'Z', 'V', 'U', 'S', 'O', 'N', 'K', 'A'}
+                };
+            }
 
             
             return new List<List<char>>();
